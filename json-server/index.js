8262ede8e99a299f8e538280a239551a6bc905e3
Trotskiy12 <mebelend33@gmail.com>
2023-05-29 18:31:02 +0300

    add json-server

---
+++ json-server/index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const server = jsonServer.create();
+
+const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
+
+server.use(jsonServer.defaults({}));
+server.use(jsonServer.bodyParser);
+
+// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
+server.use(async (req, res, next) => {
+    await new Promise((res) => {
+        setTimeout(res, 800);
+    });
+    next();
+});
+
+// Эндпоинт для логина
+server.post('/login', (req, res) => {
+    try {
+        // из тела запроса получаем логин и пароль
+        const { username, password } = req.body;
+        // подключаем базу
+        const db = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'db.json'), 'UTF-8'));
+        // из DB берем пользователей (по умолчанию пустой массив)
+        const { users = [] } = db;
+
+        // Изем пользователя в БД
+        const userFromBd = users.find(
+            // первое вхождение (find) -> сравниваем логин и пароль
+            (user) => user.username === username && user.password === password,
+        );
+
+        // если пользователь найден, то вернём его в ответ
+        if (userFromBd) {
+            return res.json(userFromBd);
+        }
+        // иначе оишбка
+        return res.status(403).json({ message: 'User not found' });
+    } catch (e) {
+        console.log(e);
+        return res.status(500).json({ message: e.message });
+    }
+});
+
+// проверяем, авторизован ли пользователь
+// eslint-disable-next-line
+server.use((req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(403).json({ message: 'AUTH ERROR' });
+    }
+
+    next();
+});
+
+server.use(router);
+
+// запуск сервера
+server.listen(8000, () => {
+    console.log('server is running on 8000 port');
+});
