f2ad3f8e937e5fcfeb310a62a97bc51b970beb30
Trotskiy12 <mebelend33@gmail.com>
2023-06-19 18:08:50 +0300

    complete 7.1

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,5 +1,6 @@
 // userReducer необходимо будет добавить в rootReducer
 // userActions понадобятся для работы с данными
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+export { getUserInited } from './model/selectors/getUserInited/getUserInited';
 export { userReducer, userActions } from './model/slice/userSlice';
 export { User, UserSchema } from './model/types/user';
